40b6f6e3e7e50a67eb69a15003a0a9247d83ba85
james2doyle <james2doyle@gmail.com>
2014-06-22 14:31:00 -0400

    simplify test. add comments

--- off.js
+++ off.js
@@ -1,7 +1,13 @@
+// use this file to turn off the LED if something goes wrong
+
+// load up the module we need
 var gpio = require("pi-gpio");
 
+// open the pin
 gpio.open(16, "output", function(err) {
+  // turn the pin off, 0 == false
   gpio.write(16, 0, function() {
+    // close this pin
     gpio.close(16);
   });
 });
--- test.js
+++ test.js
@@ -1,14 +1,19 @@
+// test to see if the LED will work
+
+// get the module
 var gpio = require("pi-gpio");
 
+// open the pin for output
 gpio.open(16, "output", function(err) {
+  // turn the pin on, 1 == true
   gpio.write(16, 1, function() {
-    gpio.close(16);
-  });
-  setTimeout(function(){
-    gpio.open(16, "output", function(err) {
+    // create a timer
+    setTimeout(function(){
+      // turn the pin off, 0 == false
       gpio.write(16, 0, function() {
+        // we are done with this pin, so close it
         gpio.close(16);
       });
-    });
-  }, 1000);
+    }, 1000); // 1000 milliseconds == 1 second
+  });
 });
